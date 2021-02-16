f445358f986bd4d96cc20c78448d2e3a5c50bfd1
Igor Kudlo <igoreklida@gmail.com>
2021-02-16 16:41:22 +0300

    feat: addBtn subBtn redux

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
+import {createStore} from 'redux'
+import {rootReducer} from './redux/rootReducer'
+
 import './styles.css'
+import {decrement, increment} from "./redux/actions";
+
+const counter = document.getElementById('counter')
+const addBtn = document.getElementById('add')
+const subBtn = document.getElementById('sub')
+const asyncBtn = document.getElementById('async')
+const themeBtn = document.getElementById('theme')
+
+const store = createStore(rootReducer, 0)
+
+addBtn.addEventListener('click', () => {
+    store.dispatch(increment())
+})
+
+subBtn.addEventListener('click', () => {
+    store.dispatch(decrement())
+})
+
+store.subscribe(() => {
+    const state = store.getState()
+
+    counter.textContent = state
+})
+
+store.dispatch({ type: 'INIT_APPLICATION' })
